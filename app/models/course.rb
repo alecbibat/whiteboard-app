8d2cf66d048f5eea1d43e6a2b91ac112abac767c
Alec Bibat <alecbibat@gmail.com>
2015-07-14 12:44:06 -0600

    add models and landing page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  has_many :user_courses
+  has_many :users, through: :user_courses
+  has_many :threads
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :threads
+  has_many :comments
+  has_many :user_courses
+  has_many :courses, through: :user_courses
 end
