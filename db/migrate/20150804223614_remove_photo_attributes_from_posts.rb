ebbdff036356dda3e7b6fcf192a692f62d7d6814
Alec <alec@alec-HP-Folio-13-Notebook-PC.(none)>
2015-08-12 14:35:28 -0500

    set up for linux environment

--- db/migrate/20150804223614_remove_photo_attributes_from_posts.rb
+++
@@ -1,8 +0,0 @@
-class RemovePhotoAttributesFromPosts < ActiveRecord::Migration
-  def change
-    remove_column :posts, :photo_file_name
-    remove_column :posts, :photo_content_type
-    remove_column :posts, :photo_file_size
-    remove_column :posts, :photo_updated_at
-  end
-end
