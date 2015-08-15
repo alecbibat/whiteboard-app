181eeefad7d8bf2bdc66eb55dccc564d3f7df3e9
Alec <alec@alec-HP-Folio-13-Notebook-PC.(none)>
2015-08-14 21:52:08 -0500

    update sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814000542) do
+ActiveRecord::Schema.define(version: 20150815023814) do
 
-  create_table "comments", force: true do |t|
+  create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -22,27 +22,28 @@ ActiveRecord::Schema.define(version: 20150814000542) do
     t.integer  "post_id"
   end
 
-  create_table "courses", force: true do |t|
-    t.string   "name"
+  create_table "courses", force: :cascade do |t|
+    t.string   "name",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "department"
+    t.string   "department",     limit: 255
     t.integer  "number"
     t.integer  "user_course_id"
+    t.text     "description"
   end
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",       limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "course_id"
-    t.string   "photo"
+    t.string   "photo",       limit: 255
   end
 
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255, null: false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -51,20 +52,20 @@ ActiveRecord::Schema.define(version: 20150814000542) do
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
 
-  create_table "subscribed_courses", force: true do |t|
+  create_table "subscribed_courses", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "username"
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_course_id"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
   end
 
 end
