c2e74d7a752b2c6fbd437bdac54d925f8f7fbd43
Alec <alec@alec-HP-Folio-13-Notebook-PC.(none)>
2015-08-21 12:50:51 -0500

    fix course_id association with assignment creation

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -10,7 +10,9 @@ end
 
   def create
     @assignment = Assignment.new(assignment_params)
-
+    binding.pry
+    @assignment_course = session[:user].subscribed_courses.find(params[:course_id])
+    @assignment.course_id = @assignment_course.course_id
     if @assignment.save
       flash[:notice] = 'Assignment saved!'
       redirect_to root_path
